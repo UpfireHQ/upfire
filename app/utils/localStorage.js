27568ca7af1876205f44142f72a073c80a1a0b28
upfiredev1 <upfiredev1@protonmail.com>
2021-10-06 21:40:11 +0200

    Fix userDataPath

--- app/utils/localStorage.js
+++ app/utils/localStorage.js
@@ -1,8 +1,20 @@
-import Store from "electron-store";
-import { random } from "./crypt/crypto.factory";
+import { app, ipcMain, ipcRenderer } from 'electron';
+import Store from 'electron-store';
+import { random } from './crypt/crypto.factory';
+
+let defaultCwd;
+if (ipcRenderer) {
+  const appData = ipcRenderer.sendSync('electron-store-get-data');
+  defaultCwd = appData.defaultCwd;
+} else if (ipcMain && app) {
+  defaultCwd = app.getPath('userData');
+} else {
+  throw new Error('localStorage: userDataPath non-determinable');
+}
+export const userDataPath = defaultCwd;
 
 export const appStore = new Store({
-  name: "preferences",
+  name: 'preferences',
   defaults: {
     statistic: { upload: 0, download: 0, seed: 0 },
     autoLaunch: false,
@@ -13,15 +25,15 @@ export const appStore = new Store({
 });
 
 export const walletStore = new Store({
-  name: "wallet",
+  name: 'wallet',
   defaults: {},
 });
 
 export const torrentStore = new Store({
-  name: "torrents",
+  name: 'torrents',
   defaults: {
     torrents: [], // {file, state, ...}
-    nodeId: random(20).toString("hex"),
-    peerId: random(20).toString("hex"),
+    nodeId: random(20).toString('hex'),
+    peerId: random(20).toString('hex'),
   },
 });
--- app/utils/logger.js
+++ app/utils/logger.js
@@ -1,13 +1,20 @@
-import electron from 'electron';
+import { app, ipcMain, ipcRenderer } from 'electron';
 import path from 'path';
 import log from 'electron-log';
 import { APP_NAME } from '../constants';
 
-if (log.transports) {
-  const userDataPath = (electron.app || electron.remote.app).getPath(
-    'userData'
-  );
+let defaultCwd;
+if (ipcRenderer) {
+  const appData = ipcRenderer.sendSync('electron-store-get-data');
+  defaultCwd = appData.defaultCwd;
+} else if (ipcMain && app) {
+  defaultCwd = app.getPath('userData');
+} else {
+  throw new Error('localStorage: userDataPath non-determinable');
+}
+const userDataPath = defaultCwd;
 
+if (log.transports) {
   log.transports.console.level = 'debug';
   log.transports.console.format = '[{level}] {text}';
 
